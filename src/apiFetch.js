9637d675244f57f518120e3f3324feacc508bf53
njoro410 <brian.njoroge@student.moringaschool.com>
2023-04-05 13:56:00 +0300

    fetch:added a fetching function to API

---
+++ src/apiFetch.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const apiFetch = async () => {
+  try {
+    const { data } = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en')
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export default apiFetch
